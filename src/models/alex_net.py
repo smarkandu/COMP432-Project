9d8290e8bb3a28f1ce78a5a6b05d1f7f9644e458
smarkandu <s_markan@hotmail.com>
2023-04-21 22:24:52 -0400

    fix: changed first linear layer input from 256 to 9216

--- src/models/alex_net.py
+++ src/models/alex_net.py
@@ -20,7 +20,7 @@ alex_net_model = torch.nn.Sequential(
     torch.nn.MaxPool2d(kernel_size=3, stride=2),
 
     torch.nn.Flatten(1, -1),
-    torch.nn.Linear(256, 9216),
+    torch.nn.Linear(9216, 9216),
     torch.nn.ReLU(),
     torch.nn.Linear(9216, 4096),
     torch.nn.ReLU(),
