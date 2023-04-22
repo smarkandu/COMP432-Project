d0c397f4e7b025b83ba0517a015541847d2223b3
smarkandu <s_markan@hotmail.com>
2023-04-21 20:56:43 -0400

    featL alext_net first layer modified such that it works for the rest of the model

--- src/models/alex_net.py
+++ src/models/alex_net.py
@@ -1,7 +1,9 @@
 import torch
 
 alex_net_model = torch.nn.Sequential(
-    torch.nn.Conv2d(1, 96, kernel_size=11, stride=4),
+    # Note: Conv2d modified from the usual first layer of AlexNet
+    # where kernel_size=11 and stride=4 in order to obtain an appropriate feature map size of 55X55X96
+    torch.nn.Conv2d(1, 96, kernel_size=12, stride=2),
     torch.nn.ReLU(),
     torch.nn.MaxPool2d(kernel_size=3, stride=2),
 
@@ -13,7 +15,7 @@ alex_net_model = torch.nn.Sequential(
     torch.nn.ReLU(),
     torch.nn.Conv2d(384, 384, kernel_size=3, padding=1),
     torch.nn.ReLU(),
-    torch.nn.Conv2d(256, 384, kernel_size=3, padding=1),
+    torch.nn.Conv2d(384, 256, kernel_size=3, padding=1),
     torch.nn.ReLU(),
     torch.nn.MaxPool2d(kernel_size=3, stride=2),
 
